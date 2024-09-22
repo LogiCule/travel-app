b7f05594fecf6477b9bef37bdd1f7e1e07d7c2a9
Rahul Dubey <rahuldubey7398@gmail.com>
2024-09-22 18:16:45 +0530

    navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-bold text-teal-500">Hello WOrld</h1>;
+  return <>home</>;
 }
---
+++ components/Button.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+interface ButtonProps {
+  title: string;
+  icon?: string;
+  type: "button" | "submit";
+  variant?: "btn_dark_green";
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${variant} `}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer ">
+        {title}
+      </label>
+    </button>
+  );
+};
+
+export default Button;
