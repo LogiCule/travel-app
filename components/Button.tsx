4ee2ead6d7259abf0074ff8e1af77d427beedbf6
Rahul Dubey <rahuldubey7398@gmail.com>
2024-09-23 10:32:33 +0530

    get app section added

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,15 +4,23 @@ interface ButtonProps {
   title: string;
   icon?: string;
   type: "button" | "submit";
-  variant?: "btn_dark_green" | "btn_green" | "btn_white_text";
+  variant?:
+    | "btn_dark_green"
+    | "btn_green"
+    | "btn_white_text"
+    | "btn_white"
+    | "btn_dark_green_outline";
+  full?: boolean;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant} `}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      } `}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap cursor-pointer ">
