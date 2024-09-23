85c923f16770803ca935e0c596f175bff1b44ce0
Rahul Dubey <rahuldubey7398@gmail.com>
2024-09-23 11:04:06 +0530

    footer added

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="">
         <Navbar />
-        <main className="relative overflow-hidden">{children}</main>
+        <main className="relative overflow-hidden scroll-smooth">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
